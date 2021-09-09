e8eaf63058a905ddaaec9362207e180a48961cdb
chojangg <83991079+chojangg@users.noreply.github.com>
2021-09-10 08:57:31 +0900

    21.03.25

---
+++ ex2.java
@@ -0,0 +1,8 @@
+public class ex2 {
+     public static void main(String[] args){
+          System.out.println("미림여자정보과학고등학교 재학");
+          System.out.println("1115");
+          System.out.println("뉴미디어소프트웨어과");
+          System.out.println("서울 거주");
+     }
+}
