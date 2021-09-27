ee908a1a8df4178101f58c1fcc945236ca394072
chojangg <83991079+chojangg@users.noreply.github.com>
2021-09-27 23:36:25 +0900

    21.04.04

---
+++ Square_1115.java
@@ -0,0 +1,7 @@
+public class Square_1115 {
+	public static void main(String args []){
+		int a = 100;
+		int b = 50;
+                            System.out.println("가로길이 : " + a + ",  " + "가로길이 : " + b + ",  " + "넓이 : " + a*b);
+	}
+}
