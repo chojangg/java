8c3802f942e96d27d4762ace6db17827cb7738fa
chojangg <83991079+chojangg@users.noreply.github.com>
2021-09-29 22:51:56 +0900

    21.04.15

---
+++ Next.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Next{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.print("문자열 입력 : ");
+		//String str=sc.next(); //문자열 입력 라인
+		String str=sc.nextLine(); //문자열 입력 라인
+		System.out.println(str);
+		
+		
+	}
+}
