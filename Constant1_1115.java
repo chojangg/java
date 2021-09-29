51fbdbe8f52f38f40cffb3640bd7c10a83ff384e
chojangg <83991079+chojangg@users.noreply.github.com>
2021-09-29 22:10:00 +0900

    21.04.09

---
+++ Constant1_1115.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Constant1_1115{
+	public static void main(String args[]){
+		Scanner scan = new Scanner(System.in);
+		String name;
+		int a;
+		int b=85;
+		System.out.print("시험점수 : ");
+		a=scan.nextInt();
+		System.out.print(a+"-"+b+"="+(a-b));
+		
+	}
+}
