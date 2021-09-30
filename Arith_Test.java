812d7c63825a3e0a4872622850c39216fd0304b2
chojangg <83991079+chojangg@users.noreply.github.com>
2021-10-01 08:30:13 +0900

    21.04.15

---
+++ Arith_Test.java
@@ -0,0 +1,16 @@
+public class Arith_Test{
+	public static void main(String args[]){
+		int answer;
+		answer=3+21/5;
+		System.out.println(answer);
+		answer=(3+21)/5;
+		System.out.println(answer);
+		answer=4+(15-7)/4;
+		System.out.println(answer);
+		answer=13/3+5*2;
+		System.out.println(answer);
+		double ans;
+		answer=5.5%2.0;
+		System.out.println(answer);
+	}
+}
