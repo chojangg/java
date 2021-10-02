eca6ebd16d411ebbebeac108248466db24fb90e0
ⒶⓁⒾⒸⒾⒶ <83991079+chojangg@users.noreply.github.com>
2021-10-03 02:36:28 +0900

    21.04.16

---
+++ BitOp1.java
@@ -0,0 +1,13 @@
+public class BitOp1{
+	public static void main(String args[]){
+		//2 -5 -7 -3 4
+		int a=2;
+		int b=-5;
+		System.out.println(a&b);
+		System.out.println(a|b);
+		System.out.println(a^b);
+		System.out.println(~1);
+		System.out.println(~b);
+		
+	}
+}
