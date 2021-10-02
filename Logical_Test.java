9920619b7a0eb5c08bd57c5c7805b358c1abd828
ⒶⓁⒾⒸⒾⒶ <83991079+chojangg@users.noreply.github.com>
2021-10-03 02:36:56 +0900

    21.04.19

---
+++ Logical_Test.java
@@ -0,0 +1,16 @@
+public class Logical_Test{
+	public static void main(String args[]){
+		int a=10;
+		System.out.println(a>10 & (a=20)==20);
+		System.out.println(a); //&&:10 &:20 l20  ll10
+		/*int a=10;
+		System.out.println((a=20)==20);
+		//System.out.println(!a);
+		System.out.println(a);
+
+		boolean power=false;
+		System.out.println(power);
+		System.out.println(!power);
+		System.out.println(!!power);*/
+	}
+}
