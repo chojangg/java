17ee692d11d5c220f26b2df5466c1d8dadcfe166
ⒶⓁⒾⒸⒾⒶ <83991079+chojangg@users.noreply.github.com>
2021-10-15 08:27:03 +0900

    21.05.20

---
+++ Multiple.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Multiple_3{
+	public static void main(String args[]){
+		System.out.print("정수 입력 : ");
+		Scanner sc=new Scanner(system.in);
+		int num=sc.nextInt();
+		if(num%3==0)//3의 배수
+			System.out.println(num + "은(는) 3의 배수입니다.");
+			System.out.println("&&&&&&&&&&&&&&&&");
+			
+	}
+}
