41361130df8e7f51c08dbcf6f737c3ee18e07080
ⒶⓁⒾⒸⒾⒶ <83991079+chojangg@users.noreply.github.com>
2021-11-26 09:30:48 +0900

    21.06.04

---
+++ Sum_Odd_Even.java
@@ -0,0 +1,11 @@
+import util.java.Scanner;
+public class Sum_Odd_Even{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.print("마지막 수 입력 : ");
+		int su=sc.nextInt();
+		//홀수합
+		 odd_sum even_sum
+		//짝수합
+	}
+}
